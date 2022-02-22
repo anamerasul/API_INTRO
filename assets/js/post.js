fc8808cf1c78c630244ce1b60601d7551eedc446
anamerasul <anamerasul@gmail.com>
2022-02-22 20:01:55 +0600

    GET, POST, PATCH, DELETE, CRUD, GET Vs POST~

--- assets/js/post.js
+++ assets/js/post.js
@@ -22,3 +22,23 @@ function displayPost(posts){
                 postContainer.appendChild(div)
         }
 }
+
+
+function AddPost(){
+        fetch('https://jsonplaceholder.typicode.com/posts',{
+                method: 'POST',
+                body: JSON.stringify({
+                  title: 'foo',
+                  body: 'bar',
+                  userId: 1,
+        }),
+        headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+              }
+})
+
+.then((res)=>res.json())
+.then((json) => console.log(json))
+
+}
+AddPost();
