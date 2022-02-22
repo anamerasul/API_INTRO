b53ff98b37b945889aacd1aa3fda03b2e080e482
anamerasul <anamerasul@gmail.com>
2022-02-22 18:32:33 +0600

    Intro to JSON, JSON Structure, parse, stringify

---
+++ assets/js/index.js
@@ -0,0 +1,33 @@
+console.log("API");
+
+// javascript object notation
+//JSON
+const user={id:11,name:"goriber amir",job:'actor'}
+// console.log(user)
+const stringified=JSON.stringify(user);
+// console.log(stringified)
+
+const shop={
+        name:'alia store',
+
+        address:'ranbir road',
+        prodducts:['laptop','mobile','pepsi'],
+        owner:{
+                name:'alia bhat',
+                profession:'actress'
+        },
+        isExpensive:false
+}
+
+// console.log(shop)
+
+const shopStringified=JSON.stringify(shop);
+
+console.log(shopStringified)
+
+
+const conveted =JSON.parse(shopStringified);
+
+console.log(conveted)
+
+
