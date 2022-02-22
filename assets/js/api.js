b639d8f89ee9adbbad0f9441313f8914937b82af
anamerasul <anamerasul@gmail.com>
2022-02-22 19:08:52 +0600

    Load more data, more APIs, send data to function

--- assets/js/api.js
+++ assets/js/api.js
@@ -9,3 +9,28 @@ fetch('https://jsonplaceholder.typicode.com/todos/1')
 
 
 }
+
+function loadusers(){
+
+        // console.log('users')
+fetch('https://jsonplaceholder.typicode.com/users')
+.then(res=>res.json())
+.then(data =>console.log(data))
+}
+
+function loadposts(){
+fetch('https://jsonplaceholder.typicode.com/posts')
+.then(res=>res.json())
+.then(data =>displayUsers(data))
+}
+
+// loadposts();
+function displayUsers(data){
+console.log(data);
+}
+
+
+
+
+
+
